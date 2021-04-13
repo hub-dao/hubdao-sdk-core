079e974afef1e21d3d71b36c77cd47fb88b0a11e
harold <harold@funit.io>
2021-04-13 15:28:56 +0900

    chainId for heco

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,8 @@ import JSBI from 'jsbi'
 export type BigintIsh = JSBI | string | number
 
 export enum ChainId {
-  MAINNET = 1,
-  ROPSTEN = 3,
-  RINKEBY = 4,
-  GÖRLI = 5,
-  KOVAN = 42
+  MAINNET = 128,
+  TESTNET = 256
 }
 
 export enum TradeType {
